577f10f659f2c6dfd6ca6d3845b1e26a4e71ce6c
Manux SPA <77162219+manuxcl@users.noreply.github.com>
2021-02-12 13:27:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Logo de Instagram</title>
+  </head>
+  <body>
+
+    <style type="text/css">
+
+      img {
+        margin: auto;
+        padding: 1%;
+        background: #d6249f;
+        background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);
+      }
+
+    </style>
+
+    <img src="instagram.png">
+
+    <!-- Vísitanos www.manux.cl -->
+
+  </body>
+</html>
